ed4461e6b608136e36917282b5be38e24956c047
Bobin Mathew <bobinm3@gmail.com>
2020-06-11 08:09:11 -0400

    Second commit

--- pong_python_game.py
+++ pong_python_game.py
@@ -1,9 +1,10 @@
 import turtle
 
 wn = turtle.Screen()
+wn.title("PING PONG by Bobin")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
-#wn.tracer(0)
+# wn.tracer(0)
 
 # Paddle A
 paddle_a = turtle.Turtle()
@@ -13,7 +14,7 @@ paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
- 
+
 # Paddle B
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
@@ -27,32 +28,37 @@ paddle_b.goto(350, 0)
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("circle")
-ball.color("white")
+ball.color("red")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 1
-ball.dy = -1
+ball.dx = 2
+ball.dy = 2
 
-# Fuction
+
+# Function
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
     paddle_a.sety(y)
+
 
 def paddle_a_down():
     y = paddle_a.ycor()
     y -= 20
     paddle_a.sety(y)
 
+
 def paddle_b_up():
     y = paddle_b.ycor()
     y += 20
     paddle_b.sety(y)
 
+
 def paddle_b_down():
     y = paddle_b.ycor()
     y -= 20
     paddle_b.sety(y)
+
 
 # Keyboard binding
 wn.listen()
@@ -62,10 +68,23 @@ wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
+# Score
+score_a = 0
+score_b = 0
+
+
+# Score Board
+board = turtle.Turtle()
+board.speed(0)
+board.color("White")
+board.penup()
+board.hideturtle()
+board.goto(0, 260)
+board.write("Player A : 0     Player B : 0", align="Center", font=("Ariel", 15, "normal"))
 
 # Main game loop
 while True:
-    #wn.update()
+    wn.update()
 
     # Move the ball
     ball.setx(ball.xcor() + ball.dx)
@@ -81,17 +100,28 @@ while True:
 
     if ball.xcor() > 380:
         ball.goto(0, 0)
-        ball.dx += 5
+        ball.dx += -5
+        score_a += 1
+        board.clear()
+        board.write("Player A : {}     Player B : {}".format(score_a, score_b), align="Center",
+                    font=("Ariel", 15, "normal"))
+
     if ball.xcor() < -380:
         ball.goto(0, 0)
         ball.dx += 5
+        score_b += 1
+        board.clear()
+        board.write("Player A : {}     Player B : {}".format(score_a, score_b), align="Center",
+                    font=("Ariel", 15, "normal"))
 
     # Paddle and ball collisions
-    if (ball.xcor() > 330 and ball.xcor() < 340) and (ball.ycor() < paddle_b.ycor() +40 and ball.ycor() > paddle_b.ycor() -40):
+    if (330 < ball.xcor() < 340) and (
+            paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(330)
         ball.dx += -5
-    
-    if (ball.xcor() < -330 and ball.xcor() > -340) and (ball.ycor() < paddle_a.ycor() +40 and ball.ycor() > paddle_a.ycor() -40):
+
+    if (-330 > ball.xcor() > -340) and (
+            paddle_a.ycor() + 40 > ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-330)
         ball.dx += 5
-    
+
